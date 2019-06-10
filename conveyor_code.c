4646a7a573a4e9e214b66eb2c8a4910675df506f
jpsalis <33296248+jpsalis@users.noreply.github.com>
2019-06-10 11:31:04 -0400

    changed sample time

--- conveyor_code.c
+++ conveyor_code.c
@@ -51,7 +51,7 @@ task main() // runs on start
 				writeDebugStreamLine("%d", count);
 			}
 
-			if(count != 0 && time1[T1] > 100)
+			if(count != 0 && time1[T1] > 200)
 			{
 				interpretCount(count);
 				count = 0;
